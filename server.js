48c91cb01478a0fbde5d2e37935f667455e9b2b8
bryantduncan98@gmail.comgit config --global user.email bryantduncan98@gmail.com <bryantduncan98@gmail.com>
2018-02-23 10:00:07 -0700

    added env

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ function initMap() {
     zoom: 5
   });
 }
+//require .env
+require("dotenv").config();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
