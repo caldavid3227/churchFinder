049e8e4ab1a4177c27a0e2f463674d9f9e7b666a
caldavi3227 <davidsoncaleb97@gmail.com>
2018-02-26 19:21:19 -0700

    final html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,7 +7,7 @@ var path = require('path');
 module.exports = function(app){
 	//set default to homepage!!!!
 	// this is the home page
-	app.get('/home', function(req, res){
+	app.get('/', function(req, res){
 		res.sendFile(path.join(__dirname, '../public/home.html'));
 	});
 	//this is the find your church page
