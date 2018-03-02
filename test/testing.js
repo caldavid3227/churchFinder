b141c4a136e30a2ba2ff991395d9e41a150f1d86
bryantduncan98@gmail.comgit config --global user.email bryantduncan98@gmail.com <bryantduncan98@gmail.com>
2018-03-02 14:02:37 -0700

    adding nitemare to run the find chruch page

---
+++ test/testing.js
@@ -0,0 +1,20 @@
+var Nightmare = require("nightmare");
+
+var nightmare = Nightmare({ show: true });
+
+nightmare
+  .goto("https://tranquil-atoll-37047.herokuapp.com/findChurch.html")
+  .type("#Zip_Code", "85234")
+  .type("#Denomination", "christian")
+  .click("#submit")
+  .evaluate(function() {
+    return document.querySelector("#links a").href;
+  })
+  .end()
+  .then(function(result) {
+    console.log(result);
+  })
+  .catch(function(error) {
+    console.error("Search failed:", error);
+  });
+
